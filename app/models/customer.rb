9351235601546246e4aa617f8b726d18434ca90c
Felipe Diesel <felipediesel@gmail.com>
2008-06-29 09:42:28 -0300

    Aula 08: has_many em customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,9 @@
 class Customer < ActiveRecord::Base
+  has_many :projects, :dependent => :destroy
+
   validates_presence_of :name
   validates_presence_of :description
   validates_uniqueness_of :name
   validates_format_of :phone, :with => /^(\d|\s)*$/
-  validates_inclusion_of :rating, :in => 1..5 
-
+  validates_inclusion_of :rating, :in => 1..5
 end
